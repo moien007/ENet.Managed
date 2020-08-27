9b56ac0b867d1e5f981fed76dbb76c494f9cc7a6
Havij khor <moien.king@gmail.com>
2020-08-27 10:21:38 +0430

    Added an unit test for IENetEventListener

---
+++ tests/ENetEventTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ENet.Managed;
+using ENet.Managed.Native;
+
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ENetEventTests
+    {
+        class Listener : IENetEventListener
+        {
+            public bool OnConnectCalled, OnDisconnectCalled, OnReceiveCalled;
+
+            public void OnConnect(ENetPeer peer, uint data)
+            {
+                OnConnectCalled = true;
+            }
+
+            public void OnDisconnect(ENetPeer peer, uint data)
+            {
+                OnDisconnectCalled = true;
+            }
+
+            public void OnReceive(ENetPeer peer, ENetPacket packet, byte channelId)
+            {
+                OnReceiveCalled = true;
+            }
+        }
+
+        [Test]
+        public void DispatchToListenerTest()
+        {
+            var listener = new Listener();
+            var ev = default(ENetEvent);
+
+            void setEvent(ENetEventType type)
+            {
+                var native = new NativeENetEvent();
+                native.Type = type;
+                ev = new ENetEvent(native);
+            }
+
+            setEvent(ENetEventType.Connect);
+            Assert.IsTrue(ev.DisptachTo(listener));
+            Assert.IsTrue(listener.OnConnectCalled);
+
+            setEvent(ENetEventType.Disconnect);
+            Assert.IsTrue(ev.DisptachTo(listener));
+            Assert.IsTrue(listener.OnDisconnectCalled);
+
+            setEvent(ENetEventType.Receive);
+            Assert.IsTrue(ev.DisptachTo(listener));
+            Assert.IsTrue(listener.OnReceiveCalled);
+
+            setEvent(ENetEventType.None);
+            Assert.IsFalse(ev.DisptachTo(listener));
+        }
+    }
+}
