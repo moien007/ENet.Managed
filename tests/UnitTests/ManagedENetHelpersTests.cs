e23b2aa9c7948fc0ca38804ba7fad956445e776d
Havij khor <moien.king@gmail.com>
2020-08-27 10:55:58 +0430

    ManagedENetHelpers unit tests added.

---
+++ tests/UnitTests/ManagedENetHelpersTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+using NUnit.Framework;
+
+using static ENet.Managed.ManagedENetHelpers;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ManagedENetHelpersTests
+    {
+        [Test]
+        public void TryGetIPEndPointTests()
+        {
+            var expectedEP = new IPEndPoint(IPAddress.Loopback, 20722);
+            Assert.IsTrue(TryGetIPEndPoint(expectedEP.ToString(), out var parsedEP));
+            Assert.AreEqual(expectedEP, parsedEP);
+
+            Assert.IsFalse(TryGetIPEndPoint("8.8.8.256:2022", out var _));
+        }
+    }
+}
